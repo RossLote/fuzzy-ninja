769ccb1a65b34bc94501052adbe5a50ff83bc63c
Ross Lote <rosslote@gmail.com>
2014-02-24 17:13:34 -0500

    test

--- fabfile.py
+++ fabfile.py
@@ -3,8 +3,8 @@ import os
 
 def prepare_deployment():
     local('python manage.py test --settings=website.test_settings')
+    local('git add . --ignore-removal', capture=True) # or local('hg add && hg commit')
     msg = raw_input('Your commit message:')
-    local('git add .') # or local('hg add && hg commit')
     local('git commit -m "{}"'.format(msg), capture=True)
     
 def watch():
