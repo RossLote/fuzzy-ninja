09ab0f4f00c6d8d02e352bf7d83722539818c516
Ross Lote <rosslote@gmail.com>
2014-02-24 17:00:57 -0500

    create a better watch

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,20 @@
 from fabric.api import local
+import os
 
 def prepare_deployment():
     local('python manage.py test --settings=website.test_settings')
     msg = raw_input('Your commit message:')
     local('git add . && git commit -m "{}"'.format(msg)) # or local('hg add && hg commit')
+    
+def watch():
+    dirs = []
+    files = [f for f in os.listdir('.')]
+    for f in files:
+        if f != '.git':
+            dirs.append(f)
+    
+    dirs = ' '.join(dirs)
+    cmd = 'when-changed -r {} -c fab prepare_deployment'.format(dirs)
+    
+    local(cmd)
+    
