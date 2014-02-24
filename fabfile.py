167d0d5a12ea104cb2292956b8b13c1093ea72a6
Ross Lote <rosslote@gmail.com>
2014-02-24 17:11:48 -0500

    test

--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,8 @@ import os
 def prepare_deployment():
     local('python manage.py test --settings=website.test_settings')
     msg = raw_input('Your commit message:')
-    local('git add . && git commit -m \'{}\''.format(msg)) # or local('hg add && hg commit')
+    local('git add .') # or local('hg add && hg commit')
+    local('git commit -m "{}"'.format(msg), capture=True)
     
 def watch():
     dirs = []
--- schedule/models/calendar.py
+++ schedule/models/calendar.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Calendar(models.Model):
     pass
+
+    def test(self):
+        pass
